fcde588932d35ceadcb3ef4f9355441b0913747b
Gav Hanna <gavhanna@gmail.com>
2019-01-05 12:29:08 +0000

    site now responsive

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,31 +1,71 @@
 import React, { Component } from 'react'
 import { NavLink } from "react-router-dom";
+import classnames from "classnames"
 
 class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isNavOpen: false
+    }
+  }
+
+  closeNav = (e) => {
+    this.setState({
+      isNavOpen: false
+    })
+  }
+
+  toggleNavOpen = e => {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
-
-        <div className="close-nav"></div>
+        <div onClick={this.closeNav}
+          className={classnames("close-nav", {
+            "open": this.state.isNavOpen
+          })}
+        ></div>
         <nav id="nav">
-          <div className="nav-button"></div>
+          <div
+            onClick={this.toggleNavOpen}
+            className={classnames("nav-button", {
+              "open": this.state.isNavOpen
+            })}
+          ></div>
           <div className="nav-wrapper">
             <div className="title">
               <span className="fancy-font">
                 <NavLink activeClassName="active" to="/">GH</NavLink>
               </span>
             </div>
-            <div className="nav-links">
-              <span className="nav-item">
+            <div
+              className={classnames("nav-links", {
+                "nav-on": this.state.isNavOpen
+              })}
+            >
+              <span
+                onClick={this.closeNav}
+                className="nav-item">
                 <NavLink activeClassName="active" to="/education">Education</NavLink>
               </span>
-              <span className="nav-item">
+              <span
+                onClick={this.closeNav}
+                className="nav-item">
                 <NavLink activeClassName="active" to="/projects">Projects</NavLink>
               </span>
-              <span className="nav-item">
+              <span
+                onClick={this.closeNav}
+                className="nav-item">
                 <NavLink activeClassName="active" to="/about">About</NavLink>
               </span>
-              <span className="nav-item">
+              <span
+                onClick={this.closeNav}
+                className="nav-item">
                 <NavLink activeClassName="active" to="/contact">Contact</NavLink>
               </span>
             </div>
